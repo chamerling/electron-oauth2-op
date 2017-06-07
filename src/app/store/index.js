3c7d63aea9320110102cf27301d4c2644be4dc8d
Christophe Hamerling <chamerling@linagora.com>
2017-06-08 01:36:44 +0200

    Add vuex logger

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createLogger from 'vuex/dist/logger';
 
 import * as actions from './actions';
 import * as types from './mutation-types';
 import mutations from './mutations';
 
 Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== 'production';
 
 export default new Vuex.Store({
   state: {
@@ -19,5 +22,6 @@ export default new Vuex.Store({
   },
   actions,
   mutations,
-  strict: process.env.NODE_ENV !== 'production'
+  strict: debug,
+  plugins: debug ? [createLogger()] : []
 })
