fd49b239edcdd9b74e7469d13ced2bc25cb5e0c0
Christophe Hamerling <chamerling@linagora.com>
2017-06-16 17:05:08 +0200

    Show title bar

--- src/app.js
+++ src/app.js
@@ -32,7 +32,6 @@ app.on('window-all-closed', () => {
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-    titleBarStyle: 'hidden',
     movable: true,
     width: 580,
     height: 365
